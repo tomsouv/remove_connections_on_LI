f4c799a6560dead52807ea8ec44a98a88a9e392f
tomsouv <14096517+tomsouv@users.noreply.github.com>
2021-06-17 06:13:08 -0700

    Update remove_connections_on_LI.py

--- remove_connections_on_LI.py
+++ remove_connections_on_LI.py
@@ -1,16 +1,19 @@
+# Name: Remove connections on LinkedIn
+# Written by tomsouv (github.com/tomsouv)
+# 06-17-2021
+
 import keyboard
 import time
 
-x = 23
+x = 25 # Change this value to however many connections you want to remove at a time.
 y = 0
-
 
 keyboard.wait('q') # This is your hotkey you press to initiate the automation.
 
 while y < x:
 
   keyboard.press_and_release('tab')
-  time.sleep(0.25)
+  time.sleep(0.25) # Change the interval to your liking, due to latency your results may vary.
   keyboard.press_and_release('tab')
   time.sleep(0.25)
   keyboard.press_and_release('tab')
